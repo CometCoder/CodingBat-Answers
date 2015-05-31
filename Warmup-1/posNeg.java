fbb4fcf53f78cc4bb275702a0a8de969e470da79
CometCoder <CometCoder@users.noreply.github.com>
2015-05-31 13:35:06 -0400

    Update posNeg.java

--- Warmup-1/posNeg.java
+++ Warmup-1/posNeg.java
@@ -1,6 +1,6 @@
 public boolean posNeg(int a, int b, boolean negative) {
   if (negative) {
-    return (a < 0 && b < 0) ? true : false;
+    return (a < 0 && b < 0);
   }
-  return ((a < 0 && b > 0) || (a > 0 && b < 0)) ? true : false;
+  return ((a < 0 && b > 0) || (a > 0 && b < 0));
 }
